549674e9e2022ca2513c2ba08cdb8404b2057762
Hana Geremew <hanaberehanu@gmail.com>
2024-09-22 20:17:07 -0400

    Added server, Axios, and ChartJS

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-console.log('Starting server...');
-
 const express = require('express');
+const path = require('path');
+const fs = require('fs'); // Add this line
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    console.log('Received request');
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/budget', (req, res) => {
+    fs.readFile(path.join(__dirname, 'budget-data.json'), 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Error reading budget data');
+        }
+        res.json(JSON.parse(data));
+    });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-}).on('error', (err) => {
-    console.error('Error starting server:', err);
+    console.log(`API served at http://localhost:${port}`);
 });
